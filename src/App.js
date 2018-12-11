42db9ce1b1b908919602fdc48cdab2f1976e4a36
ivanyufen <ivanysofficial@gmail.com>
2018-12-11 09:24:34 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+    state = { tempInput: "", inputUser: "" }
+
+    getprofPict = () => {
+        this.setState({
+            inputUser: this.state.tempInput
+        })
+    }
+
+    display() {
+        return (
+            <img className="rounded-circle bg-white my-3" src={`https://robohash.org/${this.state.inputUser}.png`} />
+        )
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <div className="container text-center p-5">
+                    <input type="text" className="form-control w-25 mx-auto my-3" placeholder="Ketik nama Anda..." onChange={(e) => { this.setState({ tempInput: e.target.value }) }}></input>
+                    <button type="button" className="btn btn-success" onClick={this.getprofPict}>Buat Profile Picture!</button>
+                    <div className="row">
+                        <div className="col-lg-12 text-center">
+                            {/* untuk validate inputUser ada isinya atau engga, kalau ada baru ditampilin */}
+                            {this.state.inputUser && this.display()}
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
 }
 
 export default App;
